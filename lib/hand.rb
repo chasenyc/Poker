15bb6d49941e9c7de637c8e77793ce40a29b1e73
App Academy Student <appacademy@Apps-Mac-mini-16.local>
2015-10-07 17:34:37 -0400

    Add straight_flush? and four_of_a_kind?

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,4 +1,5 @@
 class Hand
+
   RANKS =  {
     :straight_flush => 1,
     :four_of_a_kind => 2,
@@ -10,12 +11,15 @@ class Hand
     :one_pair => 8,
     :high_card => 9
   }
-  def compare_hands(all_hands)
 
+  attr_accessor :hand
+
+  def initialize
+    @cards = []
   end
 
   def evaluate_hand(hand)
-
+    same_suit = all_same_suit?(hand)
     frequency = count_cards(hand)
   end
 
@@ -30,5 +34,23 @@ class Hand
   def all_same_suit?(hand)
     hand.all? {|h| h.suit == hand.first.suit }
   end
+
+  def best_hand?(frequency, same_suit)
+
+
+  end
+
+  def straight_flush?(frequency, same_suit)
+      return false unless same_suit
+      return false if frequency.count != 5
+      return (frequency.keys.max - frequency.keys.min == 4)
+      false
+  end
+
+  def four_of_a_kind?(frequency)
+    frequency.values.any? {|value| value == 4}
+  end
+
+
 
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,32 +1,11 @@
 require 'hand'
 require 'card'
-# IMPLEMENT BEFORE EACH
+
 describe "Hand" do
   let (:hand) { Hand.new }
   it "is a class" do
     expect(hand).to be_an_instance_of Hand
   end
-end
-[
-  [1,2,3,4,5]
-]
-describe "Hand#evaluate_hand" do
-  let (:hand) { Hand.new }
-  let (:card_one) { Card.new(1, :hearts)}
-  let (:card_two) { Card.new(2, :spades)}
-  let (:card_three) { Card.new(3, :diamonds)}
-  let (:card_four) { Card.new(2, :clubs)}
-  let (:card_five) { Card.new(4, :spades)}
-
-  let (:one_pair) { [card_one, card_two, card_three, card_four, card_five] }
-
-  it "identifies a two-pair" do
-    expect(hand.evaluate_hand(one_pair)).to eq({:rank => 8, :highest_value => 2, :suit => nil, :cards => [1,2,3,2,4] })
-  end
-
-end
-
-describe "Hand#count_cards" do
   let (:hand) { Hand.new }
   let (:card_one) { Card.new(1, :hearts)}
   let (:card_two) { Card.new(2, :spades)}
@@ -34,24 +13,45 @@ describe "Hand#count_cards" do
   let (:card_four) { Card.new(2, :clubs)}
   let (:card_five) { Card.new(4, :spades)}
   let (:one_pair) { [card_one, card_two, card_three, card_four, card_five] }
+  let (:card_six) { Card.new(1, :hearts)}
+  let (:card_seven) { Card.new(2, :hearts)}
+  let (:card_eight) { Card.new(3, :hearts)}
+  let (:card_nine) { Card.new(2, :hearts)}
+  let (:card_ten) { Card.new(4, :hearts)}
+  let (:flush) { [card_six, card_seven, card_eight, card_nine, card_ten] }
 
-  it "returns hash of card counts" do
-    expect(hand.count_cards(one_pair)).to eq ({1=>1, 2=>2, 3=>1, 4=>1})
+  describe "Hand#count_cards" do
+
+    it "returns hash of card counts" do
+      expect(hand.count_cards(one_pair)).to eq ({1=>1, 2=>2, 3=>1, 4=>1})
+    end
+
   end
 
-end
+  describe "Hand#all_same_suit?" do
 
-describe "Hand#all_same_suit?" do
-  let (:hand) { Hand.new }
-  let (:card_one) { Card.new(1, :hearts)}
-  let (:card_two) { Card.new(2, :hearts)}
-  let (:card_three) { Card.new(3, :hearts)}
-  let (:card_four) { Card.new(2, :hearts)}
-  let (:card_five) { Card.new(4, :hearts)}
-  let (:one_pair) { [card_one, card_two, card_three, card_four, card_five] }
+    it "returns hash of card counts" do
+      expect(hand.all_same_suit?(flush)).to eq true
+    end
 
-  it "returns hash of card counts" do
-    expect(hand.all_same_suit?(one_pair)).to eq true
+  end
+  describe "Hand#straight_flush?" do
+    it "returns true if hand is a straight flush" do
+      expect(hand.straight_flush?({1 => 1, 2 => 1, 3 => 1, 4 => 1, 5 => 1}, true)).to eq true
+    end
+    it "returns false if hand is not a straight" do
+      expect(hand.straight_flush?({1 => 1, 6 => 1, 3 => 1, 4 => 1, 5 => 1}, true)).to eq false
+    end
+
   end
 
+  describe "Hand#four_of_a_kind?" do
+    it "returns true if hand has four of a kind" do
+      expect(hand.four_of_a_kind?({1 => 4, 2 => 1})).to eq true
+    end
+    it "returns false if hand has 5 different cards" do
+      expect(hand.four_of_a_kind?({1 => 1, 6 => 1, 3 => 1, 4 => 1, 5 => 1})).to eq false
+    end
+
+  end
 end
