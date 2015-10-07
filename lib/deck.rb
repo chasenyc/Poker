5ecf74874bd3b1f21ec0901b89dc7a2f4b3a69a8
App Academy Student <appacademy@Apps-Mac-mini-16.local>
2015-10-07 15:46:51 -0400

    Add populate deck and shuffle

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,22 @@
 class Deck
+  attr_reader :cards
 
+  def initialize
+    populate_deck
+  end
+
+  def populate_deck
+    @cards = []
+    suits = [:hearts, :clubs, :diamonds, :spades]
+    suits.each do |suit|
+      (1..13).each do |value|
+        @cards << Card.new(value, suit)
+      end
+    end
+    shuffle!
+  end
+
+  def shuffle!
+    cards.shuffle!
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -6,3 +6,38 @@ describe "Deck" do
     expect(deck).to be_an_instance_of Deck
   end
 end
+
+describe "Deck#cards" do
+  let (:deck) { Deck.new }
+
+  it "on Initialize, contains 52 Cards" do
+    expect(deck.cards.length).to eq 52
+    expect(deck.cards.sample).to be_an_instance_of Card
+  end
+end
+
+describe "Deck#shuffle!" do
+  let (:deck) { Deck.new }
+  let (:deck_two) { Deck.new }
+
+  it "shuffles cards" do
+    deck.shuffle!
+    deck_two.shuffle!
+    expect(deck.cards).not_to eq(deck_two.cards)
+  end
+end
+
+describe'Deck#populate_deck' do
+  let (:deck) { Deck.new }
+
+  it "populates deck and calls shuffle!" do
+    expect(deck).to receive(:shuffle!)
+    deck.populate_deck
+  end
+end
+
+
+describe 'Deck#deal_card' do
+  let (:deck) { Deck.new }
+
+end
